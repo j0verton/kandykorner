22fa058af4ca4d4837a44006f98b31fb4dc67cf2
j0verton <overtonje@gmail.com>
2020-10-05 14:59:28 -0500

    wrote list and provider for locations

---
+++ src/components/locations/LocationCard.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = () => (
+    <section className="location">
+        <div className="location__address">{location.address}</div>
+        <div className="location__size">{location.size}</div>
+        <div className="accessibility">{location.accessibility}</div>
+    </section>  
+)
---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,4 @@
+import React, { useContext, useEffect } from "react"
+import { LocationContext } from "./LocationProvider"
+import { LocationCard } from "./LocationCard"
+import "./Location.css"
